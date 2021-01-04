cb9a576ebc1cc204eac2ef7d8810bdb05b4356c2
aurelienheude <you@domain.com>
2021-01-04 16:15:37 +0100

    V1 de terminator

--- terminator/script.js
+++ terminator/script.js
@@ -4,19 +4,51 @@ function formFunction() {
     let passwordForm = document.getElementById("passwordForm").value;
     let repeatpasswordForm = document.getElementById("repeatpasswordForm").value;
 
-    if (pseudoForm != "") {
-        if (mailForm != "") {
-            if (passwordForm != "") {
-                if (repeatpasswordForm != "") {
-                    if (passwordForm === repeatpasswordForm) {
-                        document.getElementById("resultForm").innerText = ResultForm = "Valeur : " + pseudoForm + ", " + mailForm + ", " + passwordForm + ", " + repeatpasswordForm + " Bien envoyé !";
+    if (pseudoForm != "") 
+    {
+        if (mailForm != "") 
+        {
+            if (passwordForm != "") 
+            {
 
-                    } else {
-                        document.getElementById("passwordHelp").innerText = "bip bip boup bip mot de passe pas identique";
+                let lghpasswordForm = passwordForm.length;
+
+                if (lghpasswordForm <= 30) 
+                {
+                    if (lghpasswordForm >= 8) 
+                    {
+                        if (repeatpasswordForm != "") 
+                        {
+
+                            if (passwordForm === repeatpasswordForm) 
+                            {
+                                let lghMailForm = mailForm.length;
+
+                                if (lghMailForm > 4) 
+                                {
+
+                                    document.getElementById("resultForm").innerText = ResultForm = "Valeur : " + pseudoForm + ", " + mailForm + ", " + passwordForm + ", " + repeatpasswordForm + " Bien envoyé !";
+
+                                } else if (lghMailForm < 4)
+                                {
+                                    document.getElementById("emailHelp").innerText = "ton email est trop court !";
+                                }
+
+                            }else {
+                                document.getElementById("repeatpasswordHelp").innerText = "bip bip boup bip mot de passe pas identique";
+                            }
+
+                        }else {
+                            document.getElementById("passwordHelp").innerText = "champ vide !";
+                        }
+
+                    }else{
+                        document.getElementById("passwordHelp").innerText = " ton password est trop court mdr";
                     }
-                } else {
-                    document.getElementById("repeatpasswordHelp").innerText = "champ vide !";
-                }
+
+                }else{
+                    document.getElementById("passwordHelp").innerText = "Ton password est trop long. ";
+                } 
             } else {
                 document.getElementById("passwordHelp").innerText = "champ vide !";
             }
