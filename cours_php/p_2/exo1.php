2710c2fa5ce8fd716a52d67762f63604eab52b92
aurelienheude <aurelienheudepro@gmail.com>
2021-01-14 13:27:06 +0100

    exo2 p2 fait

---
+++ cours_php/p_2/exo1.php
@@ -0,0 +1,14 @@
+<?php
+/*Créer une variable age et l'initialiser avec une valeur.  
+Si l'âge est supérieur ou égale à 18, afficher Vous êtes majeur. Dans le cas contraire, afficher Vous êtes mineur.*/
+
+$age = 0;
+
+if($age >= 18)
+{
+    echo "vous êtes majeurs waou";
+}else{
+    echo "sorry bro va jouer au bille";
+}
+
+?>
