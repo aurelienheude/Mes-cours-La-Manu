ca7a55f4318f3d856646aa8089a45a1e3ebaca17
aurelienheude <aurelienheudepro@gmail.com>
2021-01-14 13:27:15 +0100

    exo 2 p2 fait

---
+++ cours_php/p_2/exo2.php
@@ -0,0 +1,28 @@
+<?php 
+
+/*Créer une variable isEasy de type booléan et l'initialiser avec une valeur.  
+Afficher C'est facile !! si c'est vrai. Dans le cas contraire afficher C'est difficile !!!. */
+
+$isEasy = true;
+
+if($isEasy == true)
+{
+    echo "c'est easy mdr";
+}else{
+    echo "c'est chaud";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
