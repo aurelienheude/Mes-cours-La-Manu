f2cd5b71cb5862d5898dc362bb1a38b8b4fba127
aurelienheude <aurelienheudepro@gmail.com>
2021-01-14 15:44:04 +0100

    exo 8 p2 fait

---
+++ cours_php/p_2/exo8.php
@@ -0,0 +1,18 @@
+<?php 
+/*Traduire ce code avec des if et des else :
+
+<?php
+  $number = 11;
+  echo ($number % 5 == 0 && $number % 3 == 0)
+  ? 'FizzBuzz'  : ( $number % 3 == 0 ? 'Fizz' : ($number % 5 == 0 ? 'Buzz' : $number));
+?>*/
+
+$number = 11;
+
+if($number % 5 == 0 && $number % 3 == 0)
+{
+    echo "FizzBuss";
+}
+
+
+?>
