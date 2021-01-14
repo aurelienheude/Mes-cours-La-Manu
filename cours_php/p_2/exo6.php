01973ea467a34c42ef8dbf20f3794fd78b9d255c
aurelienheude <aurelienheudepro@gmail.com>
2021-01-14 13:51:13 +0100

    exo6 p2

---
+++ cours_php/p_2/exo6.php
@@ -0,0 +1,17 @@
+<?php 
+
+/*Traduire ce code avec des if et des else :
+
+<?php
+  $var_a = null;
+  echo $var_a ?: 'Je suis une variable sans valeur';
+?>*/
+
+$var_a = null;
+
+if($var_a == null)
+{
+    echo "je suis une variable sans valeur";
+}
+
+?>
