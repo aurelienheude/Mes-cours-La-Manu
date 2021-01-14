21a0beb46e4d4743b29ebdb78a076f8558b6efb1
aurelienheude <aurelienheudepro@gmail.com>
2021-01-14 13:46:18 +0100

    exo5 p2

---
+++ cours_php/p_2/exo5.php
@@ -0,0 +1,42 @@
+<?php
+/*Traduire ce code avec des if et des else : 
+
+    <?php
+      echo ($gender != 'Homme') ? 'C\'est une développeuse !!!' : 'C\'est un développeur !!!';
+    ?>*/
+
+    if($gender != "homme")
+    {
+        echo "";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
