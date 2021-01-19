c61f7f0d75abf6ed6375acb3acd4ca7b3f2db801
aurelienheude <aurelienheudepro@gmail.com>
2021-01-19 13:48:16 +0100

    exo 2 p4 fait

---
+++ cours_php/p_4/exo2.php
@@ -0,0 +1,10 @@
+<?php 
+/*Faire une fonction qui prend en paramètre une chaine de caractères et qui retourne cette même chaine.*/
+
+function param_text($param)
+{
+    return $param;
+}
+
+echo param_text("agegege");
+?>
