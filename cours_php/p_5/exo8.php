dcaae827c3dfc30b4328fcc5b8db64d91beb5bcf
aurelienheude <aurelienheudepro@gmail.com>
2021-01-19 16:21:16 +0100

    exo fini

--- cours_php/p_5/exo8.php
+++ cours_php/p_5/exo8.php
@@ -1,9 +1,8 @@
 <?php 
     $months = array("janvier","février","Mars","Avril","Mai","Juin","Juin","Juillet","Aout","Septembre","Octobre","Novembre","décembre");
     
-
-    for ($nombre_ligne = 0; $nombre_ligne; $nombre_ligne++)
+    for ($i = 0; $i < count($months); $i++)
     {
-        echo $nombre_ligne[$months];
+        echo $months[$i]."\n"; // affichera $prenoms[0], $prenoms[1] etc.
     }
 ?>
