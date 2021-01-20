e16477b8bc600eead3019d9a8f6f3f80b5cbcd06
aurelienheude <aurelienheudepro@gmail.com>
2021-01-20 09:21:47 +0100

    fait

---
+++ cours_php/p_6/exo8/exo8.php
@@ -0,0 +1,81 @@
+<?php include("../../conf_html/p_high.php"); ?>
+
+
+<?php 
+
+    if(!isset($_POST["go"]))
+    {
+        $viewOn = "<div class='container d-flex vh-100'>
+                    <div class='row mx-auto align-self-center'>
+                        <div class='col-12'>
+                            <form method='POST'>
+                                <div class='form-group'>
+                                    <select class='my-2 form-group' name='civility'>
+                                        <option>Mr</option>
+                                        <option>Mme</option>
+                                    </select>
+                                </div>
+                                <div class='form-group'>
+                                    <input class='my-2 form-control' type='text' name='firstname' placeholder='votre nom'>
+                                </div>
+                                <div class='form-group'>
+                                    <input class='my-2 form-control' type='text' name='surname' placeholder='votre prenom'>
+                                </div>
+                                <div class='form-group'>
+                                    <input class='my-2 form-control' type='file' name='file' value='Envoyez mon fichier'>
+                                </div>
+                                <div class='form-group'>
+                                    <input class='my-2 form-control' type='submit' name='go' value='Validez'>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    </div>";
+
+        echo $viewOn;
+    }
+
+
+    if(isset($_POST["go"]))
+    {
+        $firstname = $_POST["firstname"];
+        $surname = $_POST["surname"];
+        $civility = $_POST["civility"];
+
+        $files = pathinfo($_POST['file']);
+
+        $viewOn = "";
+        
+        echo "Votre nom : ".$firstname.
+        "<br /> Votre prénom : ".$surname.
+        "<br /> Votre civilité : ".$civility.
+        "<br /> Nom du fichier : ".$files['basename'];
+        
+        if($files['extension'] == "pdf")
+        {
+            echo "<br /> extension du fichier : ".$files['extension'];
+        }else{
+            echo "<br /> extension du fichier : Fichier non PDF.";
+        }
+    }
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include("../../conf_html/p_low.php"); ?>
