ab24b6ed80af43000f6182cf04edf635f0926cf6
aurelienheude <aurelienheudepro@gmail.com>
2021-01-20 09:19:44 +0100

    correctif

--- cours_php/conf_html/p_high.php
+++ cours_php/conf_html/p_high.php
@@ -4,5 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercice de la manu</title>
+
+    <style>
+    body{
+        color: white;
+    }
+    *{
+        color: white;
+    }
+
+    </style>
+    <!-- bootstrap cdn -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 </head>
-<body>
+<body style="background-color: #171717;">
