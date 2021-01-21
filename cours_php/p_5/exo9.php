e9c30813a2cf1b60258981916f3fd76e3e13b1d3
aurelienheude <aurelienheudepro@gmail.com>
2021-01-21 09:12:02 +0100

    exo 9 p 5 fait

--- cours_php/p_5/exo9.php
+++ cours_php/p_5/exo9.php
@@ -1,13 +1,14 @@
 <?php 
 
-    $tableau_haut_de_france = array("02" => "aisne",
+    $tableau_haut_de_france = array(
+                                "02" => "aisne",
                                 "59" => "nord",
                                 "60" => "oise",
                                 "62" => "pas-de-calais",
                                 "80" => "Somme");
     
-    foreach($tableau_haut_de_france as $ligne)
+    foreach($tableau_haut_de_france as $value)
     {
-        echo $ligne."\n";
+        echo $value."\n";
     }
 ?>
