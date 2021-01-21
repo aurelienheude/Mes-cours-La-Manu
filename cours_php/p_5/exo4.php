b059e35955f7ac5c3cc159a636bafc8668e734f1
aurelienheude <aurelienheudepro@gmail.com>
2021-01-21 09:11:47 +0100

    exo 4 p5 fait

--- cours_php/p_5/exo4.php
+++ cours_php/p_5/exo4.php
@@ -2,7 +2,7 @@
 
     $months = array("janvier","février","Mars","Avril","Mai","Juin","Juin","Juillet","Aout","Septembre","Octobre","Novembre","décembre");
     
-    $months_aout_with_accent = array(8 =>"Août");
+    $months_aout_with_accent = array(8 => "Août");
     
     $new_months = array_replace($months, $months_aout_with_accent);
 
