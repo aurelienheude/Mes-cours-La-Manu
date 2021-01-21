5e0131de670156d56ee8c542b17220a02b5d817f
aurelienheude <aurelienheudepro@gmail.com>
2021-01-21 09:13:46 +0100

    exo 5

---
+++ cours_php/p_7/exo5/index.php
@@ -0,0 +1,32 @@
+<?php include("../../conf_html/p_high.php");
+session_start();
+
+if(isset($_POST["go"]))
+{
+    setcookie("pseudo_member", $_POST["pseudo_member"], $timestamp);
+    setcookie("password_member", $_POST["password_member"], $timestamp);
+
+    header("Location: cookie.php");
+}else{
+    $content = "<div class='container d-flex vh-100'>
+    <div class='row mx-auto align-self-center'>
+        <div class='col-12'>
+            <form method='POST' action=''>
+                <div class='form-group'>
+                    <input class='my-2 form-control' type='text' name='pseudo_member' placeholder='votre pseudo'>
+                </div>
+                <div class='form-group'>
+                    <input class='my-2 form-control' type='password' name='password_member' placeholder='●●●●●●●●●'>
+                </div>
+                <div class='form-group'>
+                    <input class='my-2 form-control' type='submit' name='go' value='Validez'>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>";
+    echo $content;
+}
+?>
+
+<?php include("../../conf_html/p_low.php");?>
