fd0b22e6550efae024ad4e5de6b1ee2b490988db
aurelienheude <aurelienheudepro@gmail.com>
2021-01-21 09:13:35 +0100

    exo 5

---
+++ cours_php/p_7/exo5/cookie.php
@@ -0,0 +1,22 @@
+<?php include("../../conf_html/p_high.php");
+session_start();
+?>
+<div class='container d-flex vh-100'>
+    <div class='row mx-auto align-self-center'>
+        <div class='col-12'>
+                <?php 
+                    if(isset($_COOKIE["pseudo_member"]) && isset($_COOKIE["password_member"]))
+                    {
+                        echo "Les cookies sont modifié ! <br /><img src='https://media.giphy.com/media/hTgCF43YwCprNzYKjN/giphy.gif' /><br />".$_COOKIE["pseudo_member"]."<br />";
+                        echo $_COOKIE["password_member"]."<br />";
+                    }else{
+                        echo "<img src='https://media.giphy.com/media/SKwcn2Sh30bEXT4Qut/giphy.gif' /><br />pas de cookies pour garry<br />";
+                    }
+                ?>
+            <a class="link" href="index.php"><- Revenir sur la page de modification des cookies</a>
+        </div>
+    </div>
+</div>
+
+
+<?php include("../../conf_html/p_low.php");?>
