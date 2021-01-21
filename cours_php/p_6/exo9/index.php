a1c11f4c7da1db3e78e2e84b280086c1244b0121
aurelienheude <aurelienheudepro@gmail.com>
2021-01-21 09:12:24 +0100

    exo fait

--- cours_php/p_6/exo9/index.php
+++ cours_php/p_6/exo9/index.php
@@ -7,43 +7,47 @@ if (!empty($_POST["go"]))
 
     $firstname = $_POST["firstname"];
     $surname = $_POST["surname"];
-    $file = $_POST["file"];
-    $civility = $_POST['civility'];
+    $civility = $_POST["civility"];
 
-    if(!empty($firstname))
+    if (!empty($firstname)) 
     {
-        if(!empty($surname))
+        if (!empty($surname)) 
         {
-            if(!empty($civility))
+            if (!empty($civility)) 
             {
-                if(!empty($file))
+                if (isset($_FILES['file']) and $_FILES['file']['error'] == 0) 
                 {
-
-                    $file = pathinfo($file);
+                            
+                    $infosfichier = pathinfo($_FILES['file']['name']);
+                    $extension_upload = $infosfichier['extension'];
+                    $extensions_autorisees = array('jpg', 'jpeg', 'png');
                     
-
-                    if ($file["extension"] == "png" OR "jpg" OR "jpeg") 
+                    if (in_array($extension_upload, $extensions_autorisees)) 
                     {
+                                
+                        move_uploaded_file($_FILES['file']['tmp_name'], 'img/' . basename($_FILES['file']['name']));
 
-                        header("Location:profile.php?profile_info=".$firstname, $surname);
+                        $tab_profil_info = ["firstname" => $firstname, 
+                                            "surname" => $surname, 
+                                            "civility" => $civility, 
+                                            "file_name" => $_FILES["file"]["tmp_name"]];
 
+                        $profil_info_str = http_build_query($tab_profil_info);
+
+                        header("Location:profile.php?firstname=".$firstname."&surname=".$surname."&civility=".$civility."&filename=".$_FILES['file']['name']);
                     }else{
-
-                        echo $file['filename']." n'est pas une image valide.";
-
+                        echo "image de profil non valide";
                     }
                 }else{
-
-                    echo "Veuillez insérer une photo de profil.";
-
+                    echo "Veuillez envoyer une image svp !";
                 }
-            }else{
+            } else {
                 echo " Veuillez rentrer une civilité.";
             }
-        }else{
+        } else {
             echo "un ou plusieurs champs vide !";
         }
-    }else{
+    } else {
         echo "un ou plusieurs champs vide !";
     }
 }
@@ -52,11 +56,11 @@ if (!empty($_POST["go"]))
 <div class='container d-flex vh-100'>
     <div class='row mx-auto align-self-center'>
         <div class='col-12'>
-            <form method='POST'>
+            <form method='POST' enctype="multipart/form-data" action="">
                 <div class='form-group'>
                     <div class="form-check">
                         <input class="form-check-input" type="radio" name="civility" value="Mr" />
-                        <label class="form-check-label" for="exampleRadios1" >
+                        <label class="form-check-label" for="exampleRadios1">
                             Mr
                         </label>
                     </div>
