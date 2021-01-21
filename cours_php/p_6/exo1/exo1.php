9ceb8c89b58d4916d0428cfd37446c32482bc89f
aurelienheude <aurelienheudepro@gmail.com>
2021-01-21 09:12:09 +0100

    exo 1 p 6

--- cours_php/p_6/exo1/exo1.php
+++ cours_php/p_6/exo1/exo1.php
@@ -1,5 +1,5 @@
 <form method="GET" action="user.php">
-    <input type="text" name="firstname" placeholder="votre nom">
-    <input type="text" name="surname" placeholder="votre prenom">
+    <input type="text" name="firstname" placeholder="votre nom" required>
+    <input type="text" name="surname" placeholder="votre prenom" required>
     <input type="submit" name="go" value="Envoyez">
 </form>
