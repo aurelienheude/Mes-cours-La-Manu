24946c10a552c7109eac17b0278c0f4863b0ef03
aurelienheude <aurelienheudepro@gmail.com>
2021-01-21 09:12:35 +0100

    exo 1 p 7 fait

---
+++ cours_php/p_7/exo1/exo1.php
@@ -0,0 +1,26 @@
+<?php include("../conf_html/p_high.php");
+
+$user_agent = $_SERVER["HTTP_USER_AGENT"];
+$ip_user = $_SERVER["REMOTE_ADDR"];
+$server_name = $_SERVER["SERVER_NAME"];
+
+echo "<p>".$user_agent."<br />".$ip_user."<br />".$server_name."</p>";
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include("../conf_html/p_low.php"); ?>
