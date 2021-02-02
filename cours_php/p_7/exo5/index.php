68386db4d6dbfc0dd049149e0356ee4b7ade9f92
aurelienheude <aurelienheudepro@gmail.com>
2021-02-02 09:48:30 +0100

    exo fait

--- cours_php/p_7/exo5/index.php
+++ cours_php/p_7/exo5/index.php
@@ -1,10 +1,9 @@
 <?php include("../../conf_html/p_high.php");
-session_start();
 
 if(isset($_POST["go"]))
 {
-    setcookie("pseudo_member", $_POST["pseudo_member"], $timestamp);
-    setcookie("password_member", $_POST["password_member"], $timestamp);
+    setcookie("pseudo_member", $_POST["pseudo_member"]);
+    setcookie("password_member", $_POST["password_member"]);
 
     header("Location: cookie.php");
 }else{
