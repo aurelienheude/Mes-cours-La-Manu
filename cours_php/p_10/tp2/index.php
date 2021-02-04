f9fae803e746f018e509f685472f568789c8aaf3
aurelienheude <aurelienheudepro@gmail.com>
2021-02-04 19:49:15 +0100

    correctif

--- cours_php/p_10/tp2/index.php
+++ cours_php/p_10/tp2/index.php
@@ -10,44 +10,6 @@ include "../../conf_html/p_high.php";
         </div>
     </div>
         <?php
-        /*
-
-    
-    Faire une page pour enregistrer un futur apprenant. On devra pouvoir saisir les informations suivantes :
-
-•             Nom
-
-•             Prénom
-
-•             Date de naissance
-
-•             Pays de naissance
-
-•             Nationalité
-
-•             Adresse
-
-•             Email
-
-•             Téléphone
-
-•             Diplôme (sans, Bac, Bac+2, Bac+3 ou supérieur)
-
-•             Numéro pôle emploi
-
-•             Nombre de badge
-
-•             Liens codecademy
-
-•             Si vous étiez un super héros/une super héroïne, qui seriez-vous et pourquoi ?
-
-•             Racontez-nous un de vos "hacks" (pas forcément technique ou informatique)
-
-•             Avez vous déjà eu une expérience avec la programmation et/ou l'informatique avant de remplir ce formulaire ?
-
-A la validation de ces informations, il faudra les afficher dans la même page à la place du formulaire.
-
-    */
 
         if (isset($_POST["go"])) 
         {
