efbf9e9608f048ce28f6b56f58418067e2ce01c2
aurelienheude <aurelienheudepro@gmail.com>
2021-03-01 11:29:48 +0100

    commit

--- cours_pdo/controllers/controller.php
+++ cours_pdo/controllers/controller.php
@@ -1,18 +1,13 @@
 <?php 
-try
-{
-    $sql = new pdo("mysql:host=localhost;dbname=colyseum;charset=utf8", "root", "");
-}catch(Exception $e)
-{
-    die('Erreur : '.$e->getMessage());
-}
 
-    $req = $sql->query("SELECT * FROM clients");
-    
-    while($data = $req->fetch())
+    declare(strict_types=1);
+
+    class sql
     {
-        echo "<li>".$data["lastName"]."</li>";
+        public function __construct()
+        { 
+            $this->dbname = define("dbname", "colyseum");
+        }
     }
 
-    $req->closeCursor();
 ?>
